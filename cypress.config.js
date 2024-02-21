2314307f77c5efa351bca597a48460c268e5d56c
sonalinimbalkar17 <sonalinimbalkar2001@gmail.com>
2024-02-21 13:59:51 +0530

    mocha awsome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on("file:preprocessor", cucumber());
     },
   },
